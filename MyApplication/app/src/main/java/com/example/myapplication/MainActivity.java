6064cebd4c66e90a5fc4ef966884ccc4031d99d0
joegrows <52843618+joegrows@users.noreply.github.com>
2020-01-25 23:05:52 -0500

    Updated logo with button
    Button is about to work i swear

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,8 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mapActivityButton = (Button)findViewById(R.id.start_button);
+        mapActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), MapsActivity.class);
+                //how to pass information
+                startActivity(startIntent);
+            }
+        });
+
     }
 
-    Button mapActivityButton = (Button)findViewById(R.id.start_button);
 
 }
