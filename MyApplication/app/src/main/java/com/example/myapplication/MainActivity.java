5fca990d0d4c620f6095085bd7d25ed23b5ff45a
joegrows <52843618+joegrows@users.noreply.github.com>
2020-01-25 22:38:00 -0500

    Logo and semi-functional location
    Map kinda works doesnt locate. logo is hype

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    Button secondActivity = (Button)findViewById(R.id.start_button);
+}
